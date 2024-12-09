b6cdeda479685a9d61863e6bf64be67e23c2272f
Cryosmium <45072703+Cryosmium@users.noreply.github.com>
2024-12-09 19:57:29 +0100

    feat: main page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,17 @@ export default function RootLayout({ children }: Readonly<{
 }>) {
     return (
         <html lang="pl" className={`${playfair.variable}`}>
-            <body className={`w-screen h-screen bg-gradient-to-bl from-primary to-secondary`}>
-                <div className={`bg-black text-white h-16`}> </div>
-                <div className={`flex justify-center content-center w-full h-full`}>
+            <body className={`w-screen h-screen bg-gradient-to-tr from-primary to-secondary overflow-clip`}>
+                {/* TODO: Header bar icons */}
+                <div className={`bg-black text-white h-16 p-1 flex gap-1`}>
+                    <div className={`bg-white h-full w-auto aspect-square`}>
+                    </div>
+                    <div className={`bg-white h-full w-auto aspect-[3/2]`}>
+                    </div>
+                    <div className={`bg-white h-full w-auto aspect-square`}>
+                    </div>
+                </div>
+                <div className={`flex justify-center content-center w-full h-full overflow-y-scroll`}>
                     {children}
                 </div>
             </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,42 @@
+// FIXME: Layout breaks on small displays, fonts and contents need to be resized and probably a min-size needs to be set
+
 export default function Home() {
   return (
     <div className={`w-4/6 h-fit m-auto`}>
-        <div>
-            <h1 className={`font-playfair font-extrabold text-black text-center text-8xl`}>Akademicka Baza Wiedzy</h1>
+        <div className={`mb-5`}>
+            <div>
+                <h1 className={`font-playfair font-extrabold text-black text-center text-8xl`}>
+                    Akademicka Baza Wiedzy
+                </h1>
+            </div>
+            <div>
+                <h4 className={`font-playfair font-extrabold text-black text-center text-2xl`}>
+                    Twoje centrum wiedzy akademickiej - znajdź publikacje, profesorów i badania w jednym miejscu!
+                </h4>
+            </div>
         </div>
-        <div>
-            <h4 className={`font-playfair font-extrabold text-black text-center text-2xl`}>
-                Twoje centrum wiedzy akademickiej - znajdź publikacje, profesorów i badania w jednym miejscu!
-            </h4>
+        <div className={`p-5 mb-5 h-60 font-[600] text-center`}>
+            <div className={`bg-black h-1/3 text-white content-center text-2xl rounded-t-3xl`}>
+                Na początku wybierz uczelnię:
+            </div>
+            <div className={`bg-black/80 h-2/3 flex text-basetext rounded-b-3xl cursor-pointer text-2xl`}>
+                <div className={`p-2 flex-1 bg-black/20 content-center rounded-bl-3xl`}>
+                    Szkoła Główna Gospodarstwa Wiejskiego
+                </div>
+                <div className={`p-2 flex-1 content-center`}>
+                    Politechnika Warszawska
+                </div>
+                <div className={`p-2 flex-1 bg-black/20 content-center rounded-br-3xl`}>
+                    Politechnika Białostocka
+                </div>
+            </div>
+        </div>
+        <div className={`flex justify-center`}>
+            <div className={
+                `w-52 h-16 m-0 bg-black font-[600] text-2xl text-white text-center content-center cursor-pointer rounded-3xl`
+            }>
+                Pomiń {`>`}
+            </div>
         </div>
     </div>
   );
