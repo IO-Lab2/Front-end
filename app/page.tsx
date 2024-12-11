e7d88c065fb42ecf1faa0a5ece1ac7ae5e79356a
Cryosmium <45072703+Cryosmium@users.noreply.github.com>
2024-12-11 20:00:40 +0100

    feat: scientist view rough layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,60 @@
 // FIXME: Layout breaks on small displays, fonts and contents need to be resized and probably a min-size needs to be set
 
 import FilterHeaderTable from "@/components/FilterHeaderTable";
+import SkipFilterButton from "@/components/SkipFilterButton";
+import {JSX, useState} from "react";
+import {UUID} from "node:crypto";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
+    const router = useRouter()
+    const [universityChoice, setUniversityChoice] = useState<UUID | null>(null);
+
+    let pageContents: JSX.Element
+
+    if (universityChoice == null) {
+        pageContents = <>
+            <div className={`mb-5`}>
+                <div>
+                    <h1 className={`font-playfair font-extrabold text-black text-center text-8xl`}>
+                        Akademicka Baza Wiedzy
+                    </h1>
+                </div>
+                <div>
+                    <h4 className={`font-playfair font-extrabold text-black text-center text-2xl`}>
+                        Twoje centrum wiedzy akademickiej - znajdź publikacje, profesorów i badania w jednym
+                        miejscu!
+                    </h4>
+                </div>
+            </div>
+            <FilterHeaderTable
+                header={`Na początku wybierz uczelnię:`}
+                onChoice={(id: UUID) => {
+                    console.log(`Wybrano uniwersytet: ${id}`)
+                    setUniversityChoice(id)
+                }}
+            />
+        </>
+    } else {
+        pageContents = <>
+            <FilterHeaderTable
+                header={`Wybierz Instytut:`}
+                parentOrg={universityChoice ?? undefined}
+                onChoice={(id: UUID) => {
+                    console.log(`Wybrano instytut: ${id}`)
+                    router.push("/view")
+                }}
+            />
+        </>
+    }
+
     return (
         <div className={`w-4/6 h-fit m-auto`}>
             <div className={`mt-10 mb-10`}>
-                <div className={`mb-5`}>
-                    <div>
-                        <h1 className={`font-playfair font-extrabold text-black text-center text-8xl`}>
-                            Akademicka Baza Wiedzy
-                        </h1>
-                    </div>
-                    <div>
-                        <h4 className={`font-playfair font-extrabold text-black text-center text-2xl`}>
-                            Twoje centrum wiedzy akademickiej - znajdź publikacje, profesorów i badania w jednym
-                            miejscu!
-                        </h4>
-                    </div>
-                </div>
-
-                <FilterHeaderTable
-                    header={`Na początku wybierz uczelnię:`}
-                    onChoice={(id) => console.log(`Wybrano ${id}`)}
-                />
+                {pageContents}
+                <SkipFilterButton onClick={() => {
+                    router.push("/view")
+                }} />
             </div>
         </div>
     );
---
+++ components/ScientistCell.tsx
@@ -0,0 +1,7 @@
+export function ScientistCell() {
+    return (
+        <div className="pl-8 pr-8 p-4 w-full h-60 even:bg-white/30">
+
+        </div>
+    )
+}
